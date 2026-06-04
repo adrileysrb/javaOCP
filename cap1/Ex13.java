3b17624cfd69753b188a41bca098a49e03bdbce9
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2026-06-04 19:04:41 -0300

    cap1: 13

---
+++ cap1/Ex13.java
@@ -0,0 +1,15 @@
+public class Ex13 {
+    Water water;
+}
+
+// A, B, C -- Lembre-se da precedência do import pelo nome em relação ao wildcard
+
+// A. import aquarium.*;
+// B. import aquarium.Water;
+//    import aquarium.jellies.*;
+// C. import aquarium.*;
+//    import aquarium.jellies.Water;
+// D. import aquarium.*;
+//    import aquarium.jellies.*;
+// E. import aquarium.Water;
+//    import aquarium.jellies.Water;
